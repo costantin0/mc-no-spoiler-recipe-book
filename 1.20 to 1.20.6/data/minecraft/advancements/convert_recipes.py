156afd14f5fb4f0b776dabdeca25dac00b82a081
costantin0 <86307814+costantin0@users.noreply.github.com>
2025-08-31 16:15:58 +0200

    Added feedback to terminal messages

--- 1.20 to 1.20.6/data/minecraft/advancements/convert_recipes.py
+++ 1.20 to 1.20.6/data/minecraft/advancements/convert_recipes.py
@@ -2,36 +2,51 @@ import os, json
 
 # Snapshot 23w16a (1.20) added the "recipe_crafted" advancement trigger
 
-print(os.getcwd())
-for root, dirs, files in os.walk("./recipes"):
+print(f"No Spoiler Recipe Book script running from '{os.getcwd()}'")
+
+recipes_folder = "./recipes"
+
+if (os.path.exists(recipes_folder)):
+    input(f"All json files in the 'recipes' subfolder will be converted, press any key to continue.\n")
+else:
+    input(f"The 'recipes' subfolder could not be found! Press any key to quit.\n")
+    quit()
+
+converted_files = 0
+
+for root, dirs, files in os.walk(recipes_folder):
     for file in files:
         if file.endswith(".json"):
             filepath = os.path.join(root, file)
-            print(filepath)
+            print(filepath, end="")
             if file.startswith("root.json"):
                 os.remove(filepath)
+                print(" - deleted")
             else:
                 with open(filepath, 'w') as f:
                     json_string = json.dumps({
-                    "parent": "minecraft:recipes/root",
-                    "criteria": {
-                        "was_crafted": {
-                            "trigger": "minecraft:recipe_crafted",
-                            "conditions": {
-                            "recipe_id": "minecraft:{}".format(file[:len(file)-5])
+                        "parent": "minecraft:recipes/root",
+                        "criteria": {
+                            "was_crafted": {
+                                "trigger": "minecraft:recipe_crafted",
+                                "conditions": {
+                                    "recipe_id": "minecraft:{}".format(file[:len(file)-5])  # remove .json
+                                }
                             }
-                        }
                         },
-                    "requirements": [
-                        [
-                        "was_crafted"
-                        ]
-                    ],
-                    "rewards": {
-                        "recipes": [
-                        "minecraft:{}".format(file[:len(file)-5])
-                        ]
-                    }
+                        "requirements": [
+                            [
+                                "was_crafted"
+                            ]
+                        ],
+                        "rewards": {
+                            "recipes": [
+                                "minecraft:{}".format(file[:len(file)-5])
+                            ]
+                        }
                     }, indent=4)
                     f.write(json_string)
-                    
+                converted_files += 1
+                print(" ✓")
+
+input(f"\nThe datapack is ready ({converted_files} files converted). Press any key to quit.\n")
